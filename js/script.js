b722dbf40ab256e9f881f2a1e1fc980c44b96626
Dani9898 <danieleoliva98@gmail.com>
2021-10-25 15:08:43 +0200

    aggiunte scritte fizz buzz e fizzbuzz con js e non con css (commentate)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 let squareCont = document.getElementById("containerSquare");
+let fizz = "fizz";
+let buzz = "buzz";
+let fizzbuzz = "fizzbuzz";
 
 
 for (let i = 1; i <= 100; i++)
 {
     if (i % 3 === 0 && i % 5 === 0 ) {
-        squareCont.innerHTML += `<div class="square multiple_15"></div>`;
+        squareCont.innerHTML += `<div class="square multiple_15">${fizzbuzz}</div>`;
     } else if (i % 5 === 0 ) {
-        squareCont.innerHTML += `<div class="square multiple_5"></div>`;
+        squareCont.innerHTML += `<div class="square multiple_5">${buzz}</div>`;
     } else if ( i % 3 === 0) {
-        squareCont.innerHTML += `<div class="square multiple_3"></div>`;
+        squareCont.innerHTML += `<div class="square multiple_3">${fizz}</div>`;
     } else {
         squareCont.innerHTML += `<div class="square">${i}</div>`;
     }
